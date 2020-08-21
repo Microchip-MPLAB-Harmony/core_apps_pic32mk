d10f98eea78d4fcedd8ff4cf5403abe6d390ebc2
Amit Raddi <amit.raddi@microchip.com>
2020-08-21 16:39:39 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_self_loopback_multi_client_pic32mk_mcj_curiosity_pro
    MH3-41654
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32mk_mcj_curiosity_pro/definitions.h
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32mk_mcj_curiosity_pro/definitions.h
@@ -51,7 +51,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
-#include "peripheral/spi/plib_spi2.h"
+#include "peripheral/spi/spi_master/plib_spi2_master.h"
 #include "driver/spi/drv_spi.h"
 #include "bsp/bsp.h"
 #include "system/int/sys_int.h"
