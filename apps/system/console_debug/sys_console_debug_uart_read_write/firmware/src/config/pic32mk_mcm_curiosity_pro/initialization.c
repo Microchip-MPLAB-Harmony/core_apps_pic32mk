facbc05d4365a3b4d736a6ecee79f187ace58351
Amit Raddi <amit.raddi@microchip.com>
2021-02-02 11:27:08 +0530

    [SYS_CONSOLE_DEBUG] Regenerated demo for system_console_debug_sys_console_debug_uart_read_write_pic32mk_mcm_curiosity_pro
    MH3-49843
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/pic32mk_mcm_curiosity_pro/initialization.c
+++ apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/pic32mk_mcm_curiosity_pro/initialization.c
@@ -92,11 +92,6 @@
 #pragma config FPLLICLK =   PLL_FRC
 #pragma config FPLLMULT =   MUL_60
 #pragma config FPLLODIV =   DIV_4
-#pragma config DSBOREN =    ON
-#pragma config DSWDTPS =    DSPS32
-#pragma config DSWDTOSC =   LPRC
-#pragma config DSWDTEN =    OFF
-#pragma config FDSEN =      ON
 #pragma config BORSEL =     HIGH
 #pragma config UPLLEN =     OFF
 
@@ -210,6 +205,7 @@ const SYS_DEBUG_INIT debugInit =
 
 void SYS_Initialize ( void* data )
 {
+
     /* Start out with interrupts disabled before configuring any modules */
     __builtin_disable_interrupts();
 
