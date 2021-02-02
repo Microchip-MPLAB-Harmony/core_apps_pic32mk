154b42019e4f5296a0da981b29e86dadee2d77ee
Amit Raddi <amit.raddi@microchip.com>
2021-02-02 11:31:05 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_pic32mk_mcm_curiosity_pro
    MH3-49846
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32mk_mcm_curiosity_pro/stdio/xc32_monitor.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32mk_mcm_curiosity_pro/stdio/xc32_monitor.c
@@ -48,16 +48,22 @@ void _mon_putc(char c);
 
 int _mon_getc(int canblock)
 {
-   volatile int c = 0;
-   while(UART6_Read((void*)&c, 1) != true);
+   int c = 0;
+   bool success = false;
+   (void)canblock;
+   do
+   {
+       success = UART6_Read(&c, 1);                
+   }while( !success);
    return c;
 }
 
 void _mon_putc(char c)
 {
-   uint8_t size = 0;
+   bool success = false;
    do
    {
-       size = UART6_Write((void*)&c, 1);
-   }while (size != 1);
+       success = UART6_Write(&c, 1);
+   }while (!success);
 }
+
