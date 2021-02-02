b23ff957c6af755ea406a8b2824a62be27984a24
Amit Raddi <amit.raddi@microchip.com>
2021-02-02 11:08:56 +0530

    [SYS_FS] Regenerated demo for fs_nvm_fat_pic32mk_mcm_curiosity_pro
    MH3-49834
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/fs/nvm_fat/firmware/src/config/pic32mk_mcm_curiosity_pro/initialization.c
+++ apps/fs/nvm_fat/firmware/src/config/pic32mk_mcm_curiosity_pro/initialization.c
@@ -92,11 +92,6 @@
 #pragma config FPLLICLK =   PLL_FRC
 #pragma config FPLLMULT =   MUL_60
 #pragma config FPLLODIV =   DIV_4
-#pragma config DSBOREN =    ON
-#pragma config DSWDTPS =    DSPS32
-#pragma config DSWDTOSC =   LPRC
-#pragma config DSWDTEN =    OFF
-#pragma config FDSEN =      ON
 #pragma config BORSEL =     HIGH
 #pragma config UPLLEN =     OFF
 
@@ -261,6 +256,7 @@ const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
 
 void SYS_Initialize ( void* data )
 {
+
     /* Start out with interrupts disabled before configuring any modules */
     __builtin_disable_interrupts();
 
