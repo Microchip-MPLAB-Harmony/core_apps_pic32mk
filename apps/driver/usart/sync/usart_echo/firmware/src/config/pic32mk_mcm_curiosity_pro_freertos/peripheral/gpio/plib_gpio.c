dcd722ea8a08f813ba9184e0af6150d45ae50ef4
Amit Raddi <amit.raddi@microchip.com>
2021-08-09 12:41:17 +0530

    [DRV_USART] Regenerated demo for driver_usart_sync_usart_echo_pic32mk_mcm_curiosity_pro_freertos
    MH3-55659
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/usart/sync/usart_echo/firmware/src/config/pic32mk_mcm_curiosity_pro_freertos/peripheral/gpio/plib_gpio.c
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/pic32mk_mcm_curiosity_pro_freertos/peripheral/gpio/plib_gpio.c
@@ -57,30 +57,25 @@
 */
 void GPIO_Initialize ( void )
 {
+
     /* PORTA Initialization */
     ANSELACLR = 0x8010; /* Digital Mode Enable */
-
     /* PORTB Initialization */
-
     /* PORTC Initialization */
-
     /* PORTD Initialization */
-
     /* PORTE Initialization */
-
     /* PORTF Initialization */
     ANSELFCLR = 0x3000; /* Digital Mode Enable */
-
     /* PORTG Initialization */
     LATG = 0x0; /* Initial Latch Value */
     TRISGCLR = 0x7000; /* Direction Control */
     ANSELGCLR = 0x800; /* Digital Mode Enable */
 
-
     /* Unlock system for PPS configuration */
     SYSKEY = 0x00000000;
     SYSKEY = 0xAA996655;
     SYSKEY = 0x556699AA;
+
     CFGCONbits.IOLOCK = 0;
 
     /* PPS Input Remapping */
@@ -90,8 +85,9 @@ void GPIO_Initialize ( void )
     RPA4R = 10;
     RPA4R = 11;
 
-    /* Lock back the system after PPS configuration */
+        /* Lock back the system after PPS configuration */
     CFGCONbits.IOLOCK = 1;
+
     SYSKEY = 0x00000000;
 
 }
