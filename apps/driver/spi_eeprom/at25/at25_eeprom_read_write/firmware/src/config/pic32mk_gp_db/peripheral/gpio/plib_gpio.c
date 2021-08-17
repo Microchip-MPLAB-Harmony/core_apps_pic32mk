82c730fcc4c6cc545d970216d4ee1384259b1f2d
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-08-17 21:00:14 +0530

    [DRV_SPI_EEPROM] Regenerated demo for driver_spi_eeprom_at25_at25_eeprom_read_write_pic32mk_gp_db
    MH3-55652
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/pic32mk_gp_db/peripheral/gpio/plib_gpio.c
+++ apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/pic32mk_gp_db/peripheral/gpio/plib_gpio.c
@@ -57,38 +57,33 @@
 */
 void GPIO_Initialize ( void )
 {
+
     /* PORTA Initialization */
     LATA = 0x800; /* Initial Latch Value */
     TRISACLR = 0x800; /* Direction Control */
     ANSELACLR = 0x802; /* Digital Mode Enable */
-
     /* PORTB Initialization */
     ANSELBCLR = 0x1; /* Digital Mode Enable */
-
     /* PORTC Initialization */
     LATC = 0x1; /* Initial Latch Value */
     TRISCCLR = 0x1; /* Direction Control */
     ANSELCCLR = 0x1; /* Digital Mode Enable */
-
     /* PORTD Initialization */
-
     /* PORTE Initialization */
-
     /* PORTF Initialization */
     ANSELFCLR = 0x3000; /* Digital Mode Enable */
     CNPUFSET = 0x3000; /* Pull-Up Enable */
-
     /* PORTG Initialization */
     LATG = 0x40; /* Initial Latch Value */
     TRISGCLR = 0x7040; /* Direction Control */
     ANSELGCLR = 0xa40; /* Digital Mode Enable */
     CNPUGSET = 0x800; /* Pull-Up Enable */
 
-
     /* Unlock system for PPS configuration */
     SYSKEY = 0x00000000;
     SYSKEY = 0xAA996655;
     SYSKEY = 0x556699AA;
+
     CFGCONbits.IOLOCK = 0;
 
     /* PPS Input Remapping */
@@ -98,8 +93,9 @@ void GPIO_Initialize ( void )
     RPB0R = 17;
     RPA1R = 17;
 
-    /* Lock back the system after PPS configuration */
+        /* Lock back the system after PPS configuration */
     CFGCONbits.IOLOCK = 1;
+
     SYSKEY = 0x00000000;
 
 }
