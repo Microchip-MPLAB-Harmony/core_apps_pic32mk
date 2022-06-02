108af05bf3b2904c2baaf4f991c73008b0a069ef
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-06-02 13:54:25 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_async_i2c_eeprom_pic32mk_gp_db
    MH3-64519
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/config/pic32mk_gp_db/initialization.c
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/config/pic32mk_gp_db/initialization.c
@@ -145,6 +145,9 @@ const DRV_I2C_PLIB_INTERFACE drvI2C0PLibAPI = {
     /* I2C PLib Transfer Write Read Add function */
     .writeRead = (DRV_I2C_PLIB_WRITE_READ)I2C_BB_WriteRead,
 
+    /*I2C PLib Transfer Abort function */
+    .transferAbort = NULL,
+
     /* I2C PLib Transfer Status function */
     .errorGet = (DRV_I2C_PLIB_ERROR_GET)I2C_BB_ErrorGet,
 
