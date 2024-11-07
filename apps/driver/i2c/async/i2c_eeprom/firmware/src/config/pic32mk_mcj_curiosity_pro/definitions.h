d136125fa6495609bef4952541f8b60d624243f3
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-11-07 19:51:24 +0530

    [DRV_] Regenerated demo for driver__i2c__async__i2c_eeprom__firmware__pic32mk_mcj_curiosity_pro.X
    MH3-103146

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/config/pic32mk_mcj_curiosity_pro/definitions.h
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/config/pic32mk_mcj_curiosity_pro/definitions.h
@@ -53,10 +53,10 @@
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
 #include "bsp/bsp.h"
+#include "driver/i2c/drv_i2c.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "driver/i2c/drv_i2c.h"
 #include "app.h"
 
 
@@ -70,13 +70,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32MK0512MCJ064"
-#define DEVICE_ARCH			 "MIPS"
-#define DEVICE_FAMILY		 "PIC32MK1570"
-#define DEVICE_SERIES		 "PIC32MK"
+#define DEVICE_NAME          "PIC32MK0512MCJ064"
+#define DEVICE_ARCH          "MIPS"
+#define DEVICE_FAMILY        "PIC32MK1570"
+#define DEVICE_SERIES        "PIC32MK"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 120000000
+#define CPU_CLOCK_FREQUENCY 120000000U
 
 // *****************************************************************************
 // *****************************************************************************
